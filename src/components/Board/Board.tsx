2fa0321b4af9f6d46aa72e87806b052aea6454fc
Pauliina Solanne <pauliina.solanne@hoxhunt.com>
2020-10-27 19:55:37 +0200

    connect api

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { pathOr } from "ramda";
 import { Card } from "../Card/Card";
 
 const BoardStyled = styled.div`
@@ -22,14 +23,45 @@ const BoardItemsStyled = styled.div`
 const content =
   "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
 
+interface ICardData {
+  name: string;
+  story: string;
+}
 export const Board = () => {
+  const [data, setData] = useState<ICardData[]>();
+  const [error, setErrors] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch("https://inklusiiv-api.herokuapp.com/api/cards");
+      data
+        .json()
+        .then((data) => setData(data))
+        .catch((err) => setErrors(err));
+    }
+
+    fetchData();
+  }, []);
+
+  console.log("data", data);
+
+  // const cardData: ICardData[] = pathOr(null, ["cards"], data);
+
+  if (!data && data) {
+    return null;
+  }
+
+  const cardData: ICardData[] | undefined = data?.map((d) => d);
+
+  console.log("cardData", cardData);
+
   return (
     <BoardStyled>
       <BoardItemsStyled>
-        <Card content={content} />
-        <Card content={content} />
-        <Card content={content} />
-        <Card content={content} />
+        {cardData &&
+          cardData.map((data, i) => (
+            <Card key={i} name={data.name} story={data.story} />
+          ))}
       </BoardItemsStyled>
     </BoardStyled>
   );
