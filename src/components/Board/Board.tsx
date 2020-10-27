def9ac48ec74fdae49680f4c3d55f68d505e9e61
Pauliina Solanne <pauliina.solanne@hoxhunt.com>
2020-10-27 22:00:04 +0200

    data from api flowing to cards

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { pathOr } from "ramda";
+import { path } from "ramda";
 import { Card } from "../Card/Card";
 
 const BoardStyled = styled.div`
@@ -19,9 +19,6 @@ const BoardItemsStyled = styled.div`
   flex-wrap: wrap;
   width: 100%;
 `;
-
-const content =
-  "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
 
 interface ICardData {
   name: string;
@@ -44,6 +41,8 @@ export const Board = () => {
     fetchData();
   }, []);
 
+  const cardData: ICardData[] | undefined = path(["data", "cards"], data);
+
   if (!data || data.length === 0) {
     return null;
   }
@@ -51,7 +50,7 @@ export const Board = () => {
   return (
     <BoardStyled>
       <BoardItemsStyled>
-        {data.map((card, i) => (
+        {cardData?.map((card, i) => (
           <Card key={i} name={card.name} story={card.story} />
         ))}
       </BoardItemsStyled>
